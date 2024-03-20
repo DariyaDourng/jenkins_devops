5ee70abed69de8e4cac45d96359627a0fef7e019
DariyaDourng <dariyadourng@gmail.com>
2024-03-20 16:21:10 +0700

    back to correct

--- src/main/java/com/jenkins/App.java
+++ src/main/java/com/jenkins/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome Dariya Dourng!");
         System.out.println("Thanks for comming here!");
-        ddd
+       
     }
 }
