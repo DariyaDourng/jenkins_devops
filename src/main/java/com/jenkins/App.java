ccd3d3b377b7dbfbf5055b91a0951c5a19105de8
DariyaDourng <dariyadourng@gmail.com>
2024-03-20 16:13:38 +0700

    make mistake to test Email Notification

--- src/main/java/com/jenkins/App.java
+++ src/main/java/com/jenkins/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome Dariya Dourng!");
         System.out.println("Thanks for comming here!");
+        ddd
     }
 }
