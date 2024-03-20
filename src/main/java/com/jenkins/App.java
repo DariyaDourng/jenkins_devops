c58e41ce429a90988fc870744a2d47cd21aba174
DariyaDourng <dariyadourng@gmail.com>
2024-03-20 12:54:02 +0700

    update some code

---
+++ src/main/java/com/jenkins/App.java
@@ -0,0 +1,8 @@
+package com.jenkins;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome Dariya Dourng!");
+        System.out.println("Thanks for comming here!");
+    }
+}
