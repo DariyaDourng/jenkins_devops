fc98a91ed99ee82a0d06b29917b1d3ff2008c5c1
DariyaDourng <dariyadourng@gmail.com>
2024-03-21 09:56:10 +0700

    auto triggered

--- src/main/java/com/jenkins/App.java
+++ src/main/java/com/jenkins/App.java
@@ -4,6 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome Dariya Dourng!");
         System.out.println("Thanks for comming here!");
+        System.out.println("Are you?");
+
        
     }
 }
