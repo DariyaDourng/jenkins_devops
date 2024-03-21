a50a004e10f3b3dd837a8841f8374c3ba340a44d
DariyaDourng <dariyadourng@gmail.com>
2024-03-21 13:08:13 +0700

    update some

--- src/main/java/com/jenkins/App.java
+++ src/main/java/com/jenkins/App.java
@@ -1,11 +1,8 @@
 package com.jenkins;
-import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Welcome Dariya!");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 200);
-        frame.setVisible(true);
+       System.err.println("Hello Dariya!");
        
     }
 }
